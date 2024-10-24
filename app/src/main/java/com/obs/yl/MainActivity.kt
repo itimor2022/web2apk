16b322d003dc53ad619a9447af5e1bd92c1960d1
tony <tony@gmail.com>
2024-10-24 16:07:27 +0700

    add

--- app/src/main/java/com/obs/yl/MainActivity.kt
+++ app/src/main/java/com/obs/yl/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var interval: Interval
 
-    private var url = "https://13.248.251.116:7102/skl002"
+    private var url = "https://154.202.206.180:501/skl001"
 
     private val gson = Gson()
     protected var mSwipeBackHelper: SwipeBackHelper? = null
